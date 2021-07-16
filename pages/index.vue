6b42bb12b6ee53d9f2b86c59352eac9729a82f4e
Kohári Levente <kohari.levente@gmail.com>
2021-07-16 11:50:36 +0200

    - Like button on the page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
+  <div class="index">
     <topnav />
+
+    <p class="text">Drop some <span>&#128077;</span></p>
+    <button v-on:click="likeThis">Like</button>
+
+    <p>The page has {{ like }}<span>&#128077;</span></p>
   </div>
 </template>
 
@@ -10,5 +15,17 @@ import topnav from '@/components/levi/topnav.vue'
 export default {
   name: 'homepage',
   components: { topnav },
+
+  data() {
+    return {
+      like: 0,
+    }
+  },
+
+  methods: {
+    likeThis() {
+      this.like++
+    },
+  },
 }
 </script>
